96ac8aad4fea3814d33a211b491b44367960425d
Nathan Dobson <nadobson@mac.com>
2026-01-20 17:05:54 -0800

    align firmware blobs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![no_std]
+use cyw43::{aligned_bytes, Aligned, A4};
 
 /// CYW43xx SoC WiFi firmware blob
 ///
@@ -7,7 +8,7 @@
 /// Please note the licensing information from the [README.md](../README.md#license), as well as the licensing
 /// information from <https://github.com/georgerobotics/cyw43-driver/tree/main/firmware>.
 #[cfg(feature = "wifi")]
-pub const CYW43_43439A0: &[u8; 231077] = include_bytes!("../dist/cyw43-firmware/43439A0.bin");
+pub const CYW43_43439A0: &Aligned<A4, [u8]> = aligned_bytes!("../dist/cyw43-firmware/43439A0.bin");
 
 /// CYW43xx SoC WiFi firmware country location matrix blob
 ///
@@ -15,7 +16,8 @@ pub const CYW43_43439A0: &[u8; 231077] = include_bytes!("../dist/cyw43-firmware/
 /// Please note the licensing information from the [README.md](../README.md#license), as well as the licensing
 /// information from <https://github.com/georgerobotics/cyw43-driver/tree/main/firmware>.
 #[cfg(feature = "wifi")]
-pub const CYW43_43439A0_CLM: &[u8; 984] = include_bytes!("../dist/cyw43-firmware/43439A0_clm.bin");
+pub const CYW43_43439A0_CLM: &Aligned<A4, [u8]> =
+    aligned_bytes!("../dist/cyw43-firmware/43439A0_clm.bin");
 
 /// CYW43xx SoC bluetooth firmware blob
 ///
@@ -23,4 +25,5 @@ pub const CYW43_43439A0_CLM: &[u8; 984] = include_bytes!("../dist/cyw43-firmware
 /// Please note the licensing information from the [README.md](../README.md#license), as well as the licensing
 /// information from <https://github.com/georgerobotics/cyw43-driver/tree/main/firmware>.
 #[cfg(feature = "bluetooth")]
-pub const CYW43_43439A0_BTFW: &[u8; 6164] = include_bytes!("../dist/cyw43-firmware/43439A0_btfw.bin");
+pub const CYW43_43439A0_BTFW: &Aligned<A4, [u8]> =
+    aligned_bytes!("../dist/cyw43-firmware/43439A0_btfw.bin");
